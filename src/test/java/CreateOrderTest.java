ab88d5d19378f4b8e9c47f978c05458d1f07678f
aidynadilkhan <aidyn9417@gmail.com>
2025-08-02 13:48:59 +0500

    - added tests for CreateOrderTest - added new class OrderRequest

--- src/test/java/CreateOrderTest.java
+++ src/test/java/CreateOrderTest.java
@@ -1,2 +1,115 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class CreateOrderTest {
+
+    private static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+    private static String accessToken;
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = BASE_URL;
+
+        String email = "hilokea@yandex.ru";
+        String password = "kassian";
+
+        Response loginResponse = given()
+                .contentType(ContentType.JSON)
+                .body("{\"email\": \"" + email + "\", \"password\": \"" + password + "\"}")
+                .when()
+                .post("/api/auth/login");
+
+        loginResponse.then().statusCode(200);
+        accessToken = loginResponse.jsonPath().getString("accessToken");
+    }
+
+    @Test
+    public void testCreateOrderWithAuthorizationAndValidIngredients() {
+        OrderRequest orderRequest = new OrderRequest(
+                List.of("61c0c5a71d1f82001bdaaa70", "61c0c5a71d1f82001bdaaa6c")
+        );
+
+        given()
+                .header("Authorization", accessToken)
+                .contentType(ContentType.JSON)
+                .body(orderRequest)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("name", notNullValue())
+                .body("order.number", notNullValue());
+    }
+
+    @Test
+    public void testCreateOrderWithoutAuthorizationWithValidIngredients() {
+        OrderRequest orderRequest = new OrderRequest(
+                List.of("61c0c5a71d1f82001bdaaa70", "61c0c5a71d1f82001bdaaa6c")
+        );
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(orderRequest)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("name", notNullValue())
+                .body("order.number", notNullValue());
+    }
+
+    @Test
+    public void testCreateOrderWithAuthorizationWithoutIngredients() {
+        OrderRequest orderRequest = new OrderRequest();
+
+        given()
+                .header("Authorization", accessToken)
+                .contentType(ContentType.JSON)
+                .body(orderRequest)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(400)
+                .body("success", is(false))
+                .body("message", equalTo("Ingredient ids must be provided"));
+    }
+
+    @Test
+    public void testCreateOrderWithoutAuthorizationWithoutIngredients() {
+        OrderRequest orderRequest = new OrderRequest();
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(orderRequest)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(400)
+                .body("success", is(false))
+                .body("message", equalTo("Ingredient ids must be provided"));
+    }
+
+    @Test
+    public void testCreateOrderWithInvalidIngredientHash() {
+        OrderRequest orderRequest = new OrderRequest(List.of("61c0c5a71d1f82001bdaaa700"));
+
+        given()
+                .header("Authorization", accessToken)
+                .contentType(ContentType.JSON)
+                .body(orderRequest)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(500);
+    }
 }
---
+++ src/test/java/OrderRequest.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class OrderRequest {
+    private List<String> ingredients;
+
+    public OrderRequest() {
+        this.ingredients = ingredients;
+    }
+
+    public OrderRequest(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
